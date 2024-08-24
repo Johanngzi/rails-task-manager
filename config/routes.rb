c2682e44c9d61ef0a9672d90707f3dac7faa72ac
johann Imbett <gazabon.johann@gmail.com>
2024-08-24 23:47:17 +0200

    pushes untracked challenges of today

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,16 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  get "tasks", to: "tasks#index"
-  # root "posts#index"
-  post "tasks", to: "tasks#create"
-  get "tasks/new", to: "tasks#new", as: :new
-  get "tasks/:id", to: "tasks#show", as: :show
-  # update the list
-  get "tasks/:id/edit", to: "tasks#edit" 
 
-  patch "tasks/:id", to: "tasks#update"
-  # patch "tasks/:id/edit", to: "tasks#edit"
+  # Defines the root path route ("/")
+  # get "tasks", to: "tasks#index"
+  # # root "posts#index"
+  # post "tasks", to: "tasks#create"
+  # get "tasks/new", to: "tasks#new", as: :new
+  # get "tasks/:id", to: "tasks#show", as: :show
+  # # update the list
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch "tasks/:id", to: "tasks#/update"
+  # delete "tasks/:id", to: "tasks#destroy"
+  resources :tasks
 end
